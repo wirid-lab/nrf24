0f819e35ba7c4df8b85587c7d267496586cc0d8f
Luis Eduardo Quibano Alarcón <lealarcon@outlook.com>
2021-07-06 11:15:31 -0500

    Create rx.ino

---
+++ rx.ino
@@ -0,0 +1,33 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+const uint64_t pipeIn = 0xE8E8F0F0E1LL; 
+RF24 radio(9, 10);
+struct MyData {
+  byte h; 
+  byte t;
+};
+MyData data;
+void setup()
+{
+  Serial.begin(9600); 
+  radio.begin();
+  radio.setAutoAck(false);
+  radio.setDataRate(RF24_250KBPS);
+  radio.openReadingPipe(1, pipeIn);
+  radio.startListening();
+}
+void recvData()
+{
+  if ( radio.available() ) {
+    radio.read(&data, sizeof(MyData));
+    }
+}
+void loop()
+{
+  recvData();
+  Serial.print("Humidity: ");
+  Serial.print(data.h);
+  Serial.print("Temperature: ");
+  Serial.print(data.t);
+}
